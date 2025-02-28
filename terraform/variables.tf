eb27755f374c56d1951848920b2db42af3641d5f
Jeremy T. Bouse <jbouse@users.noreply.github.com>
2025-02-28 21:04:06 +0000

    Update terraform to deploy website infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "domain_name" {
+  type        = string
+  description = "The domain name to host site"
+}
+
+variable "comment" {
+  description = "AWS Route53 Hosted Zone comment"
+  type        = string
+}
+
+variable "aliases" {
+  description = "Additional aliases for Cloudfront"
+  type        = list(string)
+  default     = []
+}
+
+variable "host_name" {
+  description = "The host name for the site"
+  type        = string
+  default     = null
+}
